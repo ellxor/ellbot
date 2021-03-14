37c87ea090f3ae3e9378f20ae5e97293e443a0c1
ellxor <llxoyd@gmail.com>
2021-03-14 15:17:51 +0000

    Exit if irc_connect fails

--- main.c
+++ main.c
@@ -6,12 +6,15 @@ main(int argc, char **argv)
 {
         IRC irc = {0};
 
-        irc_connect(&irc);
+        if (irc_connect(&irc) == -1)
+        {
+                fprintf(stderr, "Fatal Error: shutting down...\n");
+                exit(-1);
+        };
 
         puts("SSL Connection successful!");
         printf("%d | %p\n", irc.socket, irc.ssl);
 
         irc_disconnect(&irc);
-
         return 0;
 }
