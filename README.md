60c9ed82e832b21a5aca32b3d0455153139e7176
ellxor <llxoyd@gmail.com>
2021-03-15 00:14:36 +0000

    Add user info and connect to channel

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ ellbot is a twitch bot written in pure C.
 dependencies:  
 . posix  
 . openssl  
+  
+config.h:
+```
+#define USER <nickname>
+#define PASS <oath>
+#define CHANNEL <channel>
+```
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "config.h"
 #include "irc.h"
 
 int
@@ -14,6 +15,30 @@ main(int argc, char **argv)
 
         puts("SSL Connection successful!");
         printf("%d | %p\n", irc.socket, irc.ssl);
+
+        #define channel "JOIN "CHANNEL"\n"
+        #define nickname "NICK "USER"\n"
+        #define password "PASS "PASS"\n"
+
+        SSL_write(irc.ssl, password, sizeof(password) - 1);
+        SSL_write(irc.ssl, nickname, sizeof(nickname) - 1);
+        SSL_write(irc.ssl, channel, sizeof(channel) - 1);
+
+        char buffer[4096] = {0};
+        int count = 0;
+        int size = 0;
+
+        do
+        {
+                size = SSL_read(irc.ssl, buffer, sizeof(buffer));
+                if (size)
+                {
+                        printf("\n\nRead %d bytes:\n", size);
+                        printf("%.*s", size, buffer);
+                        count++;
+                }
+        }
+        while (count < 6);
 
         irc_disconnect(&irc);
         return 0;
